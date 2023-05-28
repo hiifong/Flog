ed093e1b240c3bedab6a51ba4881996e62632ae0
hiifong <i@hiif.ong>
2023-05-28 15:57:16 +0800

    init

---
+++ service/src/test/java/cc/hiifong/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.hiifong.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
